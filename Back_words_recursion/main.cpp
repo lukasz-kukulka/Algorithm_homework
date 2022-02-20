c90e797778847d5f3d6d4ae865d346038bacc3e2
lukasz-kukulka <qqla83@gmail.com>
2022-02-20 20:05:35 +0000

    Homework recursion done

--- Back_words_recursion/main.cpp
+++ Back_words_recursion/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-#include <string>
 
-void wordRecursion(char word[]) {
+void printCharecters(char* character) {
+    for (auto i = character; *i != '\0'; i++) {
+        std::cout << *i;
+    }
+}
 
+void wordRecursion(char* character) {
+    auto begin = character;
+    if (*character == '\0') {
+        return;
+    }
+    if (*character == ' '){
+        character++;
+        
+    } else {
+        while(*character != ' ' && *character != '\0') {
+            character++;
+        } 
+    }
+    wordRecursion(character);
+    for (auto i = begin; i < character; i++) {
+        std::cout << *i;
+    }
 }
 
 int main() {
-
+    char test[] = "aaa  bbb cccccccc  nnn zzz  ww   qqq";
+    std::cout << "BEFORE: ";
+    printCharecters(&test[0]);
+    std::cout << '\n';
+    std::cout << " AFTER: ";
+    wordRecursion(&test[0]);
+    std::cout << '\n';
     return 0;
 }
