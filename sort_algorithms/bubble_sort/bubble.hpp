3aff28158899afe986aad45313bddd84b769409c
lukasz-kukulka <qqla83@gmail.com>
2022-02-26 23:00:07 +0000

    bubble done

--- sort_algorithms/bubble_sort/bubble.hpp
+++ sort_algorithms/bubble_sort/bubble.hpp
@@ -2,5 +2,11 @@
 
 template <typename T>
 void bubble(std::vector<T>& vec) { 
-    
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = i; j < vec.size(); j++) {
+            if (vec[j] < vec[j + 1]) {
+                std::swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
 }
