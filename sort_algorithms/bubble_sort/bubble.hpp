75d15b9a285b00ec8bd4303d77e0be278fa55446
lukasz-kukulka <qqla83@gmail.com>
2022-02-27 23:17:43 +0000

    main print vec & fix bubbble

--- sort_algorithms/bubble_sort/bubble.hpp
+++ sort_algorithms/bubble_sort/bubble.hpp
@@ -3,8 +3,8 @@
 template <typename T>
 void bubble(std::vector<T>& vec) { 
     for (int i = 0; i < vec.size(); i++) {
-        for (int j = i; j < vec.size(); j++) {
-            if (vec[j] < vec[j + 1]) {
+        for (int j = 0; j < vec.size() - 1; j++) {
+            if (vec[j] > vec[j + 1]) {
                 std::swap(vec[j], vec[j + 1]);
             }
         }
--- sort_algorithms/main.cpp
+++ sort_algorithms/main.cpp
@@ -2,9 +2,17 @@
 #include "bubble_sort/bubble.hpp"
 #include <vector>
 
-template<typename T = int>
+template<typename T>
 std::vector<T> generateVector(std::vector<T>& vec, int minValue = 0, int maxValue = 100, int size = 1000) {
     return vec;
+}
+
+template<typename T>
+void printVector(std::vector<T>& vec) {
+    for (const auto & ele : vec) {
+        std::cout << ele << " ";
+    }
+    std::cout << '\n';
 }
 
 void separator() {
@@ -12,8 +20,10 @@ void separator() {
 }
 
 int main() {
-    std::vector<int> testVector(10);
+    std::vector<int> testVector{ 2, 4, 11, 4, 6, 9, 1, 112, 44, 66, 999, 7 };
+    printVector(testVector);
     testVector = generateVector(testVector, 0, 100, 1000);
     bubble(testVector);
+    printVector(testVector);
     return 0;
 }
