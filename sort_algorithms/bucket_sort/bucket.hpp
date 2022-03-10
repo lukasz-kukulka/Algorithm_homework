3f06b93227128531e382119025dc13cda5147b40
lukasz-kukulka <qqla83@gmail.com>
2022-03-10 20:13:15 +0000

    bucket sort first version some fixed

--- sort_algorithms/bucket_sort/bucket.hpp
+++ sort_algorithms/bucket_sort/bucket.hpp
@@ -1,17 +1,20 @@
 #include <algorithm>
+#include <cmath>
 #include <vector>
 
 template <typename T>
 void bucketFirst(std::vector<T>& vec) { 
-    const auto [min, max] = std::ranges::minmax_element(vec);
-    std::vector<T> tempVec(*max - *min + 1);
+    const auto [first, second] = std::ranges::minmax_element(vec);
+    const auto min { *first };
+    const auto max { *second };
+    std::vector<T> tempVec(max - min + 1);
     for (auto & ele : vec) {
-        ++tempVec[ele - *min];
+        ++tempVec[ele - min];
     }
     int iterator = 0;
     for (int i = 0; i < tempVec.size(); i++) {
         while (tempVec[i] > 0) {
-            vec[iterator++] = i + *min;
+            vec[iterator++] = i + min;
             tempVec[i]--;
         }
     }
