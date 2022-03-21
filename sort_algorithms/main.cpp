9b3cb472be77c5621e3441f207573492811adb45
lukasz-kukulka <qqla83@gmail.com>
2022-03-21 23:38:33 +0000

    quick first init

--- sort_algorithms/main.cpp
+++ sort_algorithms/main.cpp
@@ -5,6 +5,7 @@
 #include "bucket_sort/bucket.hpp"
 #include "radix_sort/radix.hpp"
 #include "merge_sort/merge.hpp"
+#include "quick_sort/quick.hpp"
 #include <vector>
 #include <random>
 
@@ -134,6 +135,16 @@ int main() {
         generateVector(testVector);
         printVector(testVector);
         mergeSecond(testVector.begin(), testVector.end() - 1);
+        printVector(testVector);
+        separator();
+    }
+
+    {
+        separator("quick sort first");
+        std::vector<int> testVector;
+        generateVector(testVector);
+        printVector(testVector);
+        quickFirst(testVector);
         printVector(testVector);
         separator();
     }
---
+++ sort_algorithms/quick_sort/quick.hpp
@@ -0,0 +1,7 @@
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+void quickFirst(std::vector<T>& vec) {
+
+}
