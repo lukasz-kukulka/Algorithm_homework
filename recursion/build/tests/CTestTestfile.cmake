3fc5d18af17a3077b13a115bab0602587fbb0748
lukasz-kukulka <qqla83@gmail.com>
2022-03-23 15:24:25 +0000

    clean repo

--- recursion/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lukasz_k/Dokumenty/Programing/Algorithm_homework/recursion/tests
-# Build directory: /home/lukasz_k/Dokumenty/Programing/Algorithm_homework/recursion/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
