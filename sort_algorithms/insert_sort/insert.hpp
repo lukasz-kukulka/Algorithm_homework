46bfd3074e2b733ae66e24939c5ea86284b63059
lukasz-kukulka <qqla83@gmail.com>
2022-04-07 14:40:05 +0100

    insert sort first version

--- sort_algorithms/insert_sort/insert.hpp
+++ sort_algorithms/insert_sort/insert.hpp
@@ -2,5 +2,11 @@
 
 template <typename T>
 void insert(std::vector<T>& vec) { 
-
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < i - 1; j++) {
+            if (vec[i] > vec[j]) {
+                std::swap(vec[i], vec[j]);
+            }
+        }
+    }
 }
