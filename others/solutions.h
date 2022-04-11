ca1309660fadae242b967918ce2089b469148799
Lukasz Kukulka <qqla83@gmail.com>
2022-04-11 11:26:22 +0100

    task three done

--- others/solutions.h
+++ others/solutions.h
@@ -2,6 +2,7 @@
 
 #include <iterator>
 #include <map>
+#include <optional>
 #include <vector>
 
 void sortByOrder( std::vector< int >& vec_to_sort, std::vector< int >const & order);
@@ -13,7 +14,7 @@ struct FrequencyData {
 
 std::map < int, int >  countFrequency( std::vector< int >const & input_vec);
 
-std::tuple<int, std::vector< int >, std::vector< int > > getBeginHightValue( std::vector< int >& input_vec);
+std::optional < int > getBeginHightValue( std::vector< int >& input_vec );
 
 void printResult( std::vector< int >const & input_vec );
 void printResult( std::map < int, int > map ); 
