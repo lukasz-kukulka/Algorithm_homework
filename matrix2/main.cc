fcd852ea7aae40935a09a1885159c020c877d7fc
lukasz-kukulka <qqla83@gmail.com>
2022-04-22 23:45:44 +0100

    Data second task init

--- matrix2/main.cc
+++ matrix2/main.cc
@@ -1,10 +1,22 @@
+#include <string>
 #include <vector>
 
 using Matrix = std::vector<std::vector<int>>;
 
+Matrix second = {{'D', 'E', 'H', 'X', 'B'},
+                 {'A', 'O', 'G', 'P', 'E'},
+                 {'D', 'D', 'C', 'F', 'D'},
+                 {'E', 'B', 'E', 'A', 'S'},
+                 {'C', 'D', 'Y', 'E', 'N'}};
+
 int countMovesColour(Matrix matrix) {
     
     return 0;
+}
+
+std::string theLongestSequence ( Matrix const & matrix ) {
+
+    return "TEST";
 }
 
 int main() {
