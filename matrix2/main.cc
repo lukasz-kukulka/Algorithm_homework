3dbd27660198fd507b065f780fec31004a0dde92
lukasz-kukulka <qqla83@gmail.com>
2022-04-26 06:03:36 +0100

    task two ready before testing

--- matrix2/main.cc
+++ matrix2/main.cc
@@ -11,9 +11,24 @@ struct Point {
     int y{};
 };
 
+
 using Matrix = std::vector<std::vector<int>>;
+using MatrixSec = std::vector<std::vector<char>>;
 
 void printMatrix( Matrix const & matrix )
+{
+    for(auto const & line : matrix)
+    {
+        for ( auto const & ele : line )
+        {
+            std::cout << "| " << ele << " |";
+        }
+        std::cout << "\n"; 
+    }
+
+}
+
+void printMatrix( MatrixSec const & matrix )
 {
     for(auto const & line : matrix)
     {
@@ -34,7 +49,7 @@ Matrix taskOneMatrix{ {-1, 0, -1, -1, -1, 0, 0 },
                       { 0, 0, -1, -1, 1, 0, 0} };
 
 
-Matrix second = {{'D', 'E', 'H', 'X', 'B'},
+MatrixSec second = {{'D', 'E', 'H', 'X', 'B'},
                  {'A', 'O', 'G', 'P', 'E'},
                  {'D', 'D', 'C', 'F', 'D'},
                  {'E', 'B', 'E', 'A', 'S'},
@@ -74,8 +89,7 @@ int countMovesColour(Matrix & matrix) {
     {
         count_moves++;
         std::queue<Point> temp = std::move(start_points);
-        std::cout << start_points.size() << std::endl;
-
+       
         while ( !temp.empty() )
         {
             for (int i{}; i < moves; ++i)
@@ -93,33 +107,40 @@ int countMovesColour(Matrix & matrix) {
     return count_moves;
 }
 
-bool outOfRange ( Matrix const & matrix, Point const & point)
+bool isReturnZero ( MatrixSec const & matrix, Point const & point, char ch)
 {
-    if ( point.x < 0 || point.y < 0 || point.x >= static_cast<int>(matrix.size()) || point.y >= static_cast<int>(matrix.front().size()) )
-    {
+    bool out_of_range{ point.x < 0 || point.y < 0 || point.x >= static_cast<int>(matrix.size()) || point.y >= static_cast<int>(matrix.front().size()) };
+    if ( ( ch != '0' && out_of_range ) || ch + 1 != matrix[point.x][point.y] )  {
         return false;
     }
     return true;
-
 }
 
-int theLongestSequenceOnePoint ( Matrix const & matrix, Point point, char ch )
+int theLongestSequenceOnePoint ( MatrixSec const & matrix, Point point, char ch )
 {
     constexpr int moves = 8;
+    int max_length { 1 };
     constexpr const std::array<int, moves> x_moves{-1, -1, -1, 0, 0, 1, 1, 1};
     constexpr const std::array<int, moves> y_moves{-1, 0, 1, 1, -1, -1, 0, 1};
 
-    
-    return x_moves[0] + y_moves[0] + matrix[0][0] + point.x + ch;
+    if ( isReturnZero(matrix, point, ch) ) {
+        return 0;
+    }
+
+    for (int i = 0; i < moves; ++i) {
+        max_length = std::max(max_length,
+                              theLongestSequenceOnePoint(matrix, Point { point.x + x_moves[i], point.y + y_moves[i] }, matrix[point.x][point.y]) + 1);
+    }
+    return max_length;
 
 }
 
-int theLongestSequence ( Matrix const & matrix ) {
+int theLongestSequence ( MatrixSec const & matrix ) {
     int max_sequence{};
 
     for ( int x{}; x < static_cast < int > ( matrix.size() ); ++x )
     {
-        for ( int y{}; y < static_cast < int > ( matrix.size() ); ++y )
+        for ( int y{}; y < static_cast < int > ( matrix[x].size() ); ++y )
         {
             max_sequence = std::max( max_sequence, theLongestSequenceOnePoint(matrix, Point{x, y}, '0' ) );
         }
@@ -128,12 +149,16 @@ int theLongestSequence ( Matrix const & matrix ) {
     return max_sequence;
 }
 
-
-
 int main() {
     printMatrix(taskOneMatrix);
     std::cout << countMovesColour( taskOneMatrix ) << '\n';
     printMatrix(taskOneMatrix);
+    std::cout << "--------------------------------------------------------------\n\n";
+    printMatrix(second);
+    std::cout << theLongestSequence(second) << '\n';
     return 0;
     
 }
+
+
+
