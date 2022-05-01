331d569dfc56c137360dc96cf5c0fe209fa6fa87
Lukasz Kukulka <qqla83@gmail.com>
2022-05-01 05:39:18 +0100

    prepare data for task one

--- others/solutions.h
+++ others/solutions.h
@@ -1,3 +1,23 @@
 #pragma once
 
 #include <vector>
+
+namespace Homework{
+
+    namespace TaskOne {
+        std::vector< int > toSort {1,2,3,4,5,6};
+        std::vector< int > order {4,2,7,8,1};
+        void sortByOrder( std::vector< int > vec_to_sort, std::vector< int > order);
+
+    } // namespace TaskOne
+
+    namespace TaskTwo {
+
+    } // namespace TaskTwo
+
+    namespace TaskThree {
+
+
+    } // namespace TaskThree
+
+} // namespace Homework
