6aeadafd38cdca134d104456d62f4e3cdd259a9c
Lukasz Kukulka <qqla83@gmail.com>
2022-05-05 06:05:29 +0100

    prepared solutions.cc file

--- others/solutions.cc
+++ others/solutions.cc
@@ -1,13 +1,30 @@
 #include "solutions.h"
 
-// 1. Chapter 9: Sort element by order presented in second table. In both tables there could be any values. You need to focus only on valid values and make sort operation.If there is no value in `order` table you need to sort it at the end of `toSort` vector.
-// - Example input: `toSort {1,2,3,4,5,6}` `order {4,2,7,8,1}`
-// - Example output: `{4,2,1,3,5,6}`
+namespace Homework{
 
-// 2. Chapter 13: Find frequency of each value in sorted vector
-// - Example input: `1,1,1,1,2,2,2,3,3,4,5,5,5`
-// - Example output: `{{1,4},{2,3},{3,2},{4,1},{5,3}}`
+    namespace TaskOne {
+        // 1. Chapter 9: Sort element by order presented in second table. In both tables there could be any values. You need to focus only on valid values and make sort operation.If there is no value in `order` table you need to sort it at the end of `toSort` vector.
+        // - Example input: `toSort {1,2,3,4,5,6}` `order {4,2,7,8,1}`
+        // - Example output: `{4,2,1,3,5,6}`
+        void sortByOrder( std::vector< int > vec_to_sort, std::vector< int > order);
 
-// 3. Chapter 15: Find element matching predicate. You need to find a value for which all values on left are lower and on the right are higher.
-// - Example input: `2,3,1,8,7,10,14,12`
-// - Example output: <h>10</h> -> values `2,3,1,8,7` are lower and `14,12` are higher 
+    } // namespace TaskOne
+
+    namespace TaskTwo {
+        // 2. Chapter 13: Find frequency of each value in sorted vector
+        // - Example input: `1,1,1,1,2,2,2,3,3,4,5,5,5`
+        // - Example output: `{{1,4},{2,3},{3,2},{4,1},{5,3}}`
+        std::vector< FrequencyData > countFrequency( std::vector< int > input_vec);
+    } // namespace TaskTwo
+
+    namespace TaskThree {
+        // 3. Chapter 15: Find element matching predicate. You need to find a value for which all values on left are lower and on the right are higher.
+        // - Example input: `2,3,1,8,7,10,14,12`
+        // - Example output: <h>10</h> -> values `2,3,1,8,7` are lower and `14,12` are higher 
+        std::iterator<int> getBeginHightValue( std::vector< int > input_vec );
+
+    } // namespace TaskThree
+    void printResult( std::vector< int > input_vec, std::iterator< int > iterator = nullptr );
+    
+
+} // namespace Homework
