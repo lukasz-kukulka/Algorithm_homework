85b93403a294070c05c7cad377fcf113929ff24a
Lukasz Kukulka <qqla83@gmail.com>
2022-05-08 06:05:10 +0100

    declaration fun task two

--- others_2/solution.h
+++ others_2/solution.h
@@ -2,17 +2,13 @@
 
 #include <vector>
 
-std::vector< int > input_vec { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-void tripletReverse(std::vector<int> input_vec, int begin, int end );
+std::vector< int > triplet_reserve_vec { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+const std::vector< int > find_subsequence_vec { 1, 2, 3, 4, 6, 7, 8, 9, 10, 11 };
 
-// 2. Chapter 6: Find longest subsequence, which contain following numbers from range. A range is a sequence of numbers `0 1 2 3...` 
-// The numbers don't need to be in sorted order, it could be `3 4 5 0 1 2`. Range don't need to start form <h>0</h>, it could be `5 6 7 8 9`.
-// - Example input: `5, 0, 4, 7, 1, 2, 8, 5, 3, 4`
-// - Example output: `0, 1, 2, 3, 4, 5` (in any order)
+void tripletReverse(std::vector<int>& input_vec, const int begin, const int end );
 
-// - Example input2: `0 1 2 3 4 6 7 8 9 10 11`
-// - Example output: `6 7 8 9 10 11` (in any order)
 
+std::vector<int> findSubsequence( std::vector<int> input_vec );
 
 // 3. Chapter 10: Sort a table with given order with memoory complexity <h>O(1)</h>. First table contain numbers and second contain indexes. 
 // You need to sort first table by reoredering all elements from first table to index presented on second table. 
