46e12bfb22e36530e60beabbe5d20a1e09e5e0d3
Lukasz Kukulka <qqla83@gmail.com>
2022-05-11 06:27:55 +0100

    prepare solution.cc file

--- others_2/solution.cc
+++ others_2/solution.cc
@@ -10,6 +10,11 @@
 // - Example input2: vec: `1 2 3 4 5 6 7 8 9`, begin: <h>2</h> end: <h>8</h>.
 // - Example output2: `1 2 5 4 3 8 7 6 9` (we reverse 6 numbers, because in range 2-9 there are two full triplets).
 //                         b           e
+void tripletReverse(std::vector<int>& input_vec, const int begin, const int end )
+{
+
+}
+
 
 // 2. Chapter 6: Find longest subsequence, which contain following numbers from range. A range is a sequence of numbers `0 1 2 3...` 
 // The numbers don't need to be in sorted order, it could be `3 4 5 0 1 2`. Range don't need to start form <h>0</h>, it could be `5 6 7 8 9`.
@@ -18,6 +23,10 @@
 
 // - Example input2: `0 1 2 3 4 6 7 8 9 10 11`
 // - Example output: `6 7 8 9 10 11` (in any order)
+std::vector<int> findSubsequence( std::vector<int> const& input_vec )
+{
+
+}
 
 
 // 3. Chapter 10: Sort a table with given order with memoory complexity <h>O(1)</h>. First table contain numbers and second contain indexes. 
@@ -27,3 +36,12 @@
 
 // - Example input: vec: `{1,2,3,4,5,6}` | order `{2,4,3,5,1,0}`
 // - Example output:  `6 5 1 3 2 4`
+void sortWithOrder(std::vector<int>& input_vec, std::vector<int> const& order )
+{
+
+}
+
+void printVec( std::vector<int> const& vec )
+{
+    
+}
