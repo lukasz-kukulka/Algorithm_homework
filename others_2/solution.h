e69347053a395e630f25804f9e279a116f414704
Lukasz Kukulka <qqla83@gmail.com>
2022-06-10 01:01:54 +0100

    declaration print fun

--- others_2/solution.h
+++ others_2/solution.h
@@ -10,11 +10,5 @@ std::vector< int > order { 2, 4, 3, 5, 1, 0 };
 void tripletReverse(std::vector<int>& input_vec, const int begin, const int end );
 std::vector<int> findSubsequence( std::vector<int> const& input_vec );
 void sortWithOrder(std::vector<int>& input_vec, std::vector<int> const& order );
-// 3. Chapter 10: Sort a table with given order with memory complexity O(1). First table contain numbers and second contain indexes. 
-// You need to sort first table by reoredering all elements from first table to index presented on second table. 
-// For instance tab1 `1 2 3` order `2 0 1`, so first element <h>1</h> need to be on <h>2</h> index, 
-// <h>2</h> element need to be on <h>0</h> index and <h>3</h> element need to be on <h>1</h> index. Result: `2 3 1`.
 
-// - Example input: vec: `{1,2,3,4,5,6}` | order `{2,4,3,5,1,0}`
-// - Example output:  `6 5 1 3 2 4`
-
+void printVec( std::vector<int> const& vec );
